54e553ef7eaf169a3e2fb65dd39e35e85752935c
miyasitatakeru <k021g1291@g.neec.ac.jp>
2022-06-24 11:56:23 +0900

    06_01.深度テスト

--- main.cpp
+++ main.cpp
@@ -204,6 +204,61 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		device->CreateRenderTargetView(backBuffers[i], &rtvDesc, rtvHandle);
 	}
 
+	//リソース設定
+	D3D12_RESOURCE_DESC depthResourceDesc{};
+	depthResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	//レンダーターゲットビューに合わせる
+	depthResourceDesc.Width = window_width;
+	//レンダーターゲットビューに合わせる
+	depthResourceDesc.Height = window_height;
+	depthResourceDesc.DepthOrArraySize = 1;
+	//震度値フォーマット
+	depthResourceDesc.Format = DXGI_FORMAT_D32_FLOAT;
+	depthResourceDesc.SampleDesc.Count = 1;
+	//デプスステンシル
+	depthResourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+	//震度値用ヒーププロパティ
+	D3D12_HEAP_PROPERTIES depthHeapProp{};
+	depthHeapProp.Type = D3D12_HEAP_TYPE_DEFAULT;
+	//深度値のクリア設定
+	D3D12_CLEAR_VALUE depthClearValue{};
+	//深度値1.0f(最大値)でクリア
+	depthClearValue.DepthStencil.Depth = 1.0f;
+	//深度値フォーマット
+	depthClearValue.Format = DXGI_FORMAT_D32_FLOAT;
+
+	//リソース生成
+	ID3D12Resource* depthBuff = nullptr;
+	result = device->CreateCommittedResource(
+		&depthHeapProp,
+		D3D12_HEAP_FLAG_NONE,
+		&depthResourceDesc,
+		//深度値書き込みに使用
+		D3D12_RESOURCE_STATE_DEPTH_WRITE,
+		&depthClearValue,
+		IID_PPV_ARGS(&depthBuff)
+	);
+
+	//深度ビュー用デスクリプタヒープ作成
+	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc{};
+	//深度ビューは一つ
+	dsvHeapDesc.NumDescriptors = 1;
+	//デプスステンシルビュー
+	dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+	ID3D12DescriptorHeap* dsvHeap = nullptr;
+	result = device->CreateDescriptorHeap(&dsvHeapDesc, IID_PPV_ARGS(&dsvHeap));
+
+	//深度ビュー作成
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+	//深度フォーマット
+	dsvDesc.Format = DXGI_FORMAT_D32_FLOAT;
+	dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+	device->CreateDepthStencilView(
+		depthBuff,
+		&dsvDesc,
+		dsvHeap->GetCPUDescriptorHandleForHeapStart()
+	);
+
 	// フェンスの生成
 	ID3D12Fence* fence = nullptr;
 	UINT64 fenceVal = 0;
@@ -244,19 +299,61 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	};
 	// 頂点データ
 	Vertex vertices[] = {
-		//       x          y        z           u      v
-		{{  -50.0f,  -50.0f,  0.0f}, {0.0f, 1.0f}}, // 左下
-		{{  -50.0f,   50.0f,  0.0f}, {0.0f, 0.0f}}, // 左上
-		{{   50.0f,  -50.0f,  0.0f}, {1.0f, 1.0f}}, // 右下
-		{{   50.0f,   50.0f,  0.0f}, {1.0f, 0.0f}}, // 右上
+		//      x        y        z          u      v
+		//前
+		{{  -5.0f,  -5.0f,  -5.0f}, {0.0f, 1.0f}}, // 左下
+		{{  -5.0f,   5.0f,  -5.0f}, {0.0f, 0.0f}}, // 左上
+		{{   5.0f,  -5.0f,  -5.0f}, {1.0f, 1.0f}}, // 右下
+		{{   5.0f,   5.0f,  -5.0f}, {1.0f, 0.0f}}, // 右上
+		//後
+		{{  -5.0f,  -5.0f,   5.0f}, {0.0f, 1.0f}}, // 左下
+		{{  -5.0f,   5.0f,   5.0f}, {0.0f, 0.0f}}, // 左上
+		{{   5.0f,  -5.0f,   5.0f}, {1.0f, 1.0f}}, // 右下
+		{{   5.0f,   5.0f,   5.0f}, {1.0f, 0.0f}}, // 右上
+		//左
+		{{  -5.0f,  -5.0f,  -5.0f}, {0.0f, 1.0f}}, // 左下
+		{{  -5.0f,  -5.0f,   5.0f}, {0.0f, 0.0f}}, // 左上
+		{{  -5.0f,   5.0f,  -5.0f}, {1.0f, 1.0f}}, // 右下
+		{{  -5.0f,   5.0f,   5.0f}, {1.0f, 0.0f}}, // 右上
+		//右
+		{{   5.0f,  -5.0f,  -5.0f}, {0.0f, 1.0f}}, // 左下
+		{{   5.0f,  -5.0f,   5.0f}, {0.0f, 0.0f}}, // 左上
+		{{   5.0f,   5.0f,  -5.0f}, {1.0f, 1.0f}}, // 右下
+		{{   5.0f,   5.0f,   5.0f}, {1.0f, 0.0f}}, // 右上
+		//下
+		{{  -5.0f,  -5.0f,  -5.0f}, {0.0f, 1.0f}}, // 左下
+		{{  -5.0f,  -5.0f,   5.0f}, {0.0f, 0.0f}}, // 左上
+		{{   5.0f,  -5.0f,  -5.0f}, {1.0f, 1.0f}}, // 右下
+		{{   5.0f,  -5.0f,   5.0f}, {1.0f, 0.0f}}, // 右上
+		//上
+		{{  -5.0f,   5.0f,  -5.0f}, {0.0f, 1.0f}}, // 左下
+		{{  -5.0f,   5.0f,   5.0f}, {0.0f, 0.0f}}, // 左上
+		{{   5.0f,   5.0f,  -5.0f}, {1.0f, 1.0f}}, // 右下
+		{{   5.0f,   5.0f,   5.0f}, {1.0f, 0.0f}}, // 右上
 	};
 	float angle = 0.0f;//カメラの回転角
 
 	// インデックスデータ
 	unsigned short indices[] = {
+		//前
 		0, 1, 2, // 三角形1つ目
 		1, 2, 3, // 三角形2つ目
-	};
+		//後
+		4, 5, 6, // 三角形3つ目
+		5, 6, 7, // 三角形4つ目
+		//左
+		8, 9, 10, // 三角形5つ目
+		9, 10, 11, // 三角形6つ目
+		//右
+		12, 13, 14, // 三角形7つ目
+		13, 14, 15, // 三角形8つ目
+		//下
+		16, 17, 18, // 三角形9つ目
+		17, 18, 19, // 三角形10つ目
+		//上
+		20, 21, 22, // 三角形9つ目
+		21, 22, 23, // 三角形10つ目
+	}; //続き スライド12から
 
 
 	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
@@ -416,6 +513,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	// グラフィックスパイプライン設定
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
+	//デプスシンプルステートの設定
+	//深度テストを行う
+	pipelineDesc.DepthStencilState.DepthEnable = true;
+	//書き込みを許可
+	pipelineDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+	//小さければ合格
+	pipelineDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
+	//深度値フォーマット
+	pipelineDesc.DSVFormat = DXGI_FORMAT_D32_FLOAT;
 
 	// シェーダーの設定
 	pipelineDesc.VS.pShaderBytecode = vsBlob->GetBufferPointer();
@@ -526,7 +632,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//ビュー変換行列
 	XMMATRIX matView;
 	//視点座標
-	XMFLOAT3 eye(0, 0, -100);
+	XMFLOAT3 eye(0, 50, -100);
 	//注視点座標
 	XMFLOAT3 target(0, 0, 0);
 	//上方向ベクトル
@@ -596,7 +702,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	ScratchImage scratchImg{};
 	// WICテクスチャのロード
 	result = LoadFromWICFile(
-		L"Resources/texture.png",   //「Resources」フォルダの「texture.png」
+		L"Resources/texture2.png",   //「Resources」フォルダの「texture.png」
 		WIC_FLAGS_NONE,
 		&metadata, scratchImg);
 	ScratchImage mipChain{};
@@ -852,7 +958,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		// レンダーターゲットビューのハンドルを取得
 		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
 		rtvHandle.ptr += bbIndex * device->GetDescriptorHandleIncrementSize(rtvHeapDesc.Type);
-		commandList->OMSetRenderTargets(1, &rtvHandle, false, nullptr);
+		D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle = dsvHeap->GetCPUDescriptorHandleForHeapStart();
+		commandList->OMSetRenderTargets(1, &rtvHandle, false, &dsvHandle);
 
 		// 3.画面クリア R G B A
 		FLOAT clearColor[] = { 0.1f, 0.25f, 0.5f, 1.0f }; // 青っぽい色
@@ -861,6 +968,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			clearColor[0] = 1.0f;
 		}
 		commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+		commandList->ClearDepthStencilView(dsvHandle, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
 
 		// 4.描画コマンド
 		// ビューポート設定コマンド
